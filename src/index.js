b9d015a98887007cfabed8fe8ef1ea66f845c048
William Moreira da Silva <william.moreirasilva@hotmail.com>
2023-04-12 01:27:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+
+// import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import App from './App';
 
+// TODO - teste smell
 ReactDOM.render(
   <React.StrictMode>
     <App />
