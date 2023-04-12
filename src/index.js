56b3f0f23bb5a6a6291b74e1628df5680ff7bd16
William Moreira da Silva <william.moreirasilva@hotmail.com>
2023-04-12 01:33:04 -0300

    index.js - remove task and codesmells

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-
-// import React from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import App from './App';
 
-// TODO - teste smell
 ReactDOM.render(
   <React.StrictMode>
     <App />
